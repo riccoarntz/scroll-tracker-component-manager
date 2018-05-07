0e69f86323eb0f1a4ec4b37f4307d1504f7fa6f9
riccoarntz <ricco@mediamonks.com>
2018-05-07 15:52:27 +0200

    Make ScrollComponentManagerOptions not required

--- src/lib/interface/IScrollTrackerComponentManagerOptions.ts
+++ src/lib/interface/IScrollTrackerComponentManagerOptions.ts
@@ -1,16 +1,16 @@
 export interface IScrollTrackerComponentManagerOptions {
-  element: string;
-  methods: {
-    transitionIn: string;
-    transitionOut: string;
-    startLoopingAnimation: string;
-    stopLoopingAnimation: string;
+  element?: string;
+  methods?: {
+    transitionIn?: string;
+    transitionOut?: string;
+    startLoopingAnimation?: string;
+    stopLoopingAnimation?: string;
   };
-  vars: {
-    transitionInThreshold: string;
-    componentId: string;
+  vars?: {
+    transitionInThreshold?: string;
+    componentId?: string;
   };
-  config: {
+  config?: {
     setDebugLabel?: boolean;
     debugBorderColor?: string;
     resizeDebounce?: number;
